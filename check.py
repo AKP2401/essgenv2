ba5f6c7e305fcaecd8f8ac490a6b553005dcd3e6
AKP2401 <aadhash2001@gmail.com>
2021-10-31 12:10:56 +0530

    ASD

--- check.py
+++
@@ -1,9 +0,0 @@
-import requests
-from readability import Document
-
-g = requests.get("https://realpython.com/python-web-scraping-practical-introduction/").text
-gg = Document(g)
-
-print(gg.title())
-with open("asd.txt", 'w') as f:
-    f.write(gg.summary())
--- run.py
+++ run.py
@@ -1,34 +1,41 @@
-import urllib
 from googs import Googs
 from bs4 import BeautifulSoup
-from adremove import AdRemover
-import re, requests
-import lxml.html
-import urllib.request
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class Run():
     def __init__(self) -> None:
-        ADBLOCK_RULES = ['https://easylist-downloads.adblockplus.org/ruadlist+easylist.txt',  
-                 'https://filters.adtidy.org/extension/chromium/filters/1.txt']
+        # ADBLOCK_RULES = ['https://easylist-downloads.adblockplus.org/ruadlist+easylist.txt',  
+        #          'https://filters.adtidy.org/extension/chromium/filters/1.txt']
         self.paragraph = ""
-        rule_files = [link.rpartition('/')[-1] for link in ADBLOCK_RULES]
-        for rule_url, rule_file in zip(ADBLOCK_RULES, rule_files):
-            r = requests.get(rule_url)
-            with open(rule_file, 'w', encoding="utf-8") as f:
-                print(r.text, file=f)
-        self.remover = AdRemover(*rule_files)
+        # rule_files = [link.rpartition('/')[-1] for link in ADBLOCK_RULES]
+        # for rule_url, rule_file in zip(ADBLOCK_RULES, rule_files):
+        #     r = requests.get(rule_url)
+        #     with open(rule_file, 'w', encoding="utf-8") as f:
+        #         print(r.text, file=f)
+        # self.remover = AdRemover(*rule_files)
 
     def __scrape(self, link ):
-        html = requests.get(link).text
-        document = lxml.html.document_fromstring(html)
-        self.remover.remove_ads(document)
-        clean_html = lxml.html.tostring(document)
-        print(clean_html)
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+        driver = webdriver.Chrome(options=options)
+        driver.get(link)
+        with open("out/Python.txt", "w+", encoding="utf-8") as f:
+            para = driver.find_elements_by_id("bodyContent")
+            p_tags = para.find_elements_by_tag_name("p")
+
+            for p_tag in para:
+                f.write(f"\t{p_tag.text}\n\n")
+            f.close()
+
+
 
     def run(self, topic:str, limit:int=5):
-        links = Googs().search(topic=topic, limit=limit)
-        print("Fetch Done")
+        # links = Googs().search(topic=topic, limit=limit)
+        # print("Fetch Done")
+        links = ["https://en.wikipedia.org/wiki/Python_(programming_language)"]
         for i in range(limit):
             self.__scrape(links[i])
         print("Scraping Done")
